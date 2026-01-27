ed7b380f4f4667270f91583158883d9c07a9ff44
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2026-01-27 08:48:33 -0300

    feat(info): links a items de info

--- src/app/pages/info/info.page.ts
+++ src/app/pages/info/info.page.ts
@@ -27,6 +27,14 @@ import {
   imports: [IonItem, IonIcon, IonList, IonLabel, IonListHeader, IonContent],
 })
 export class InfoPage {
+  readonly helpUrl = 'https://github.com/matigaleanodev/foodly-notes/issues';
+  readonly githubUrl = 'https://github.com/matigaleanodev';
+  readonly linkedinUrl = 'https://www.linkedin.com/in/matias-galeano';
+  readonly contactEmail = 'contacto@matiasgaleano.com.ar';
+  readonly spoonacularUrl = 'https://spoonacular.com/food-api';
+  readonly privacyUrl = 'https://matiasgaleano.dev/foodly-notes/privacy';
+  readonly termsUrl = 'https://matiasgaleano.dev/foodly-notes/terms';
+
   constructor() {
     addIcons({
       mailOutline,
